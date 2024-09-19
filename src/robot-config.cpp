d13740607cf8022e35899a42d4842760b7f1a6f5
Anduril-X <lucas.klimkowski2026@gmail.com>
2024-09-19 14:03:35 -0400

    gh

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -3,3 +3,10 @@
 
 using namespace vex;
 
+//Initialize Devices
+motor frontLeft = motor(PORT1, ratio6_1, false);
+motor middleLeft = motor(PORT2, ratio6_1, false);
+motor backLeft = motor(PORT3, ratio6_1, false);
+motor frontRight = motor(PORT4, ratio6_1, false);
+motor middleRight = motor(PORT5, ratio6_1, false);
+motor backRight = motor(PORT6, ratio6_1, false);
